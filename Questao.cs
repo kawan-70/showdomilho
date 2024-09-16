8c2cee85a73c6faaad00760985db721a1419049a
showdomilho <kauan220656@gmail.com>
2024-09-16 11:52:56 -0300

    00002

---
+++ Questao.cs
@@ -0,0 +1,77 @@
+namespace showdomilho;
+public class Questao
+
+    {
+        public string Pergunta;
+        public string Resposta0;
+        public string Resposta1;
+        public string Resposta2;
+        public string Resposta3;
+        public string Resposta4;
+        public int levelresposta;
+        public int Respostascorreta;
+//________________________________________
+        private Label labelPergunta;
+        private Button BtnResposta0;
+        private Button BtnResposta1;
+        private Button BtnResposta2;
+        private Button BtnResposta3;
+        private Button BtnResposta4;
+    }
+//________________________________________
+    public void Desenho()
+        {
+            LabelPergunta.Text = Pergunta;
+            BtnResposta0.Text = resposta0;
+            BtnResposta1.Text = resposta1;
+            BtnResposta2.Text = resposta2;
+            BtnResposta3.Text = resposta3;
+            BtnResposta4.Text = resposta4;
+        }
+//________________________________________
+
+        public void Quetão()
+        {
+        }
+
+    public void Quetão(label Pergunta,lp butoon Button0,botton1,button2,button3,button4)
+        {
+            labelPergunta = lp
+            BtnResposta0 = buttonresposta0;
+            BtnResposta1 = buttonresposta1;
+            BtnResposta2 = buttonresposta2;
+            BtnResposta3 = buttonresposta3;
+            BtnResposta4 = buttonresposta4;
+        }
+
+         public void Comfigurarestruturadodesenho(label Pergunta,lp butoon Button0,botton1,button2,button3,button4)
+        {
+            labelPergunta = lp
+            BtnResposta0 = buttonresposta0;
+            BtnResposta1 = buttonresposta1;
+            BtnResposta2 = buttonresposta2;
+            BtnResposta3 = buttonresposta3;
+            BtnResposta4 = buttonresposta4;
+        }
+         
+        public bool Verificarresposta(int Respondido)
+        {
+         if (respondidoCerta== Respondido)
+        {
+            var btn = qualbtn(RR);
+            btn bacroudColor.Color Green
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+
+        Privat Button QualBtn (int Respondido)
+        {
+            if (Respondidocerto == 0)
+            return if  (Respostascorreta == 0)
+        }
+        } 
+
+        
