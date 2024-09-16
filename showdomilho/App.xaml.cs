d068f8a5b06695dbd305ff557efa1c33be200332
showdomilho <kauan220656@gmail.com>
2024-09-16 09:30:14 -0300

    000000001

---
+++ showdomilho/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace showdomilho;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ showdomilho/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace showdomilho;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
