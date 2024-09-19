a9523e82c512b2952afe6899eb5ba9967e62bad8
showdomilho <kauan220656@gmail.com>
2024-09-19 09:04:19 -0300

    qwerty

---
+++ showdomilho/Gerenciador.cs
@@ -0,0 +1,12 @@
+//namespace showdomilho;
+//public class Gerenciador
+//
+//{
+//    List<Questao>ListaQuestão=new List<Questao> ();
+//    List<int>quetaoResolvidas = new List<int>();
+//    Questao Questaocorrente;
+//     
+//     public Gerenciador (Label LPergunta, buttonResposta0, buttonResposta1, buttonResposta2,buttonResposta3, buttonResposta4)
+//            CriaPeergunta (LP)
+//
+//}
